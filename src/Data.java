454e630043d0f9f12569e0dc5c4609b678048f4d
谢昊 <799522476@qq.com>
2019-10-15 20:27:20 +0800

    2019.10.15 20:27 改了个注释

--- src/Data.java
+++ src/Data.java
@@ -6,7 +6,7 @@ import java.util.Map;
  */
 public class Data {
     public HashMap<String, Integer> mapSet = new HashMap<String,Integer>();//记录预设,key存名字,value存编号
-    public HashMap<Integer, String[]> result = new HashMap<Integer,String[]>();//记录结果,key存名字,value存类型和描述
+    public HashMap<Integer, String[]> result = new HashMap<Integer,String[]>();//记录结果,key存顺序,value存类型和描述
 
     public Data(FileOperator fileOperator) throws Exception{
         String str;
